a7f5deb859131db88ab1d1d12f1f1a39d423d089
Sudy <128252727+sudy-super@users.noreply.github.com>
2024-07-03 22:13:26 +0900

    Update embedding_model.py
    attention

--- embedding_model.py
+++ embedding_model.py
@@ -3,6 +3,8 @@ from sentence_transformers import SentenceTransformer
 class CreateEmbedding:
     def __init__(self):
         self.model_multi = SentenceTransformer('sentence-transformers/paraphrase-multilingual-mpnet-base-v2')
+        # intfloat/multilingual-e5-largeの方が高性能
+        # けどsettingsの数値のチューニングだるいから放置
         print("[CHECKPOINT] CreateEmbedding System Startup!")
     def creating_embedding(self, sentences):
         embedding_multi = self.model_multi.encode(sentences)
